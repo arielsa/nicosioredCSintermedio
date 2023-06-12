5b3e99dc36233a7208b700064158d4a27ac2a443
ariel <santa.cruz.unla@gmail.com>
2023-06-11 21:53:43 -0300

    delegados

---
+++ cap_10_DelegadosII/CRefri.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap_10_DelegadosII
+{
+    internal class CRefri
+    {
+        //Aqui se declaran los delegados
+        public delegate void DReservasBajas(int pKilos);
+        public delegate void DDescongelado(int pGrados);
+
+        private int kilosAlimentos = 0;
+        private int grados = 0;
+
+        //Variables que usan los delegados
+        private DReservasBajas delReservas;
+        private DDescongelado delDescongelado;
+
+        public CRefri(int pKilos, int pGrados)
+        {
+            kilosAlimentos = pKilos;
+            grados = pGrados;
+        }
+        public void AdicionaMetodoReservas(DReservasBajas pMetodo)
+        {
+            delReservas = pMetodo;
+        }
+        public void AdicionaMetodoDescongelado(DDescongelado pMetodo)
+        {
+            delDescongelado = pMetodo;
+        }
+        //propiedades para controlar los atributos
+        public int Kilos { get { return kilosAlimentos; } }
+        public int Grados { get { return grados; } }
+
+        //metodo que simula el trabajo del refri
+        public void Trabajar(int pConsumo)
+        {
+            kilosAlimentos -= pConsumo;
+            grados += 1;
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("{0} kilos, {1} grados", kilosAlimentos, grados);
+
+            //Verificamos si se cumple la condicion de los delegados
+            if (kilosAlimentos < 10)
+            {
+                //Invocamos el delegado
+                delReservas(kilosAlimentos);
+            }
+            if (grados > 0)
+            {
+                //Invocamos el delegado
+                delDescongelado(grados);
+            }
+        }
+    }
+}
---
+++ cap_10_DelegadosII/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap_10_DelegadosII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creamos el refri
+            CRefri miRefri = new CRefri(70, -20);
+            Random rnd = new Random();
+
+            //adicionamos los handlers
+            miRefri.AdicionaMetodoReservas(new CRefri.DReservasBajas(InformeKilos));
+            miRefri.AdicionaMetodoDescongelado(new CRefri.DDescongelado(InformeGrados));
+
+            //el refri trabajando
+            while (miRefri.Kilos > 0)
+            {
+                miRefri.Trabajar(rnd.Next(1, 5));
+            }
+        }
+        public static void InformeKilos(int pKilos )
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Informe de Kilos");
+            Console.WriteLine("Kilos: {0}", pKilos);
+        }
+        public static void InformeGrados(int pGrados)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Informe de grados");
+            Console.WriteLine("grados: {0}", pGrados);
+        }
+    }
+}
